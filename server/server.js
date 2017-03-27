6701a15765ac480a91ff8e9ee5e0f18c18faa811
omar aragon <omar aragon>
2017-03-27 00:25:26 -0500

    frontend setup

--- server/server.js
+++ server/server.js
@@ -1,24 +1,33 @@
 const express = require('express'),
+  http = require('http'),
   hbs = require('hbs'),
   path = require('path'),
   logger = require('morgan'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log("new user connected");
+});
 
-app.listen(port, () => {
+app.get('*', (req,res) => {
+  res.sendfile('./public/index/html');
+});
+
+server.listen(port, () => {
   console.log(`listening on port ${port}...`);
 
 })
